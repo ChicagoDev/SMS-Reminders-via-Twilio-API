8d5dac30944a041aff525d0a0547a958e797d883
Blake Gideon <bgideon@luc.edu>
2014-12-22 15:44:23 -0600

    Changed CORs headers

--- RestSMS.js
+++ RestSMS.js
@@ -9,10 +9,9 @@ var server = restify.createServer({
 
 //CORS
 server.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "http://localhost:8000");
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-    res.header("Access-Control-Max-Age", "1000");
     next();
 });
 
