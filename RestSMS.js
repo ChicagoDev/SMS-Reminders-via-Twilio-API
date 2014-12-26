1571810df33fc48f8312da5304c1eb7097b45651
Blake Gideon <bgideon@luc.edu>
2014-12-26 12:53:48 -0600

    Fixed Cors issue. We can only use one CORS header for now. So for deployment we will use the forgetfulfairy.com domain header.

--- RestSMS.js
+++ RestSMS.js
@@ -7,11 +7,9 @@ var server = restify.createServer({
     version: '0.0.1'
 })
 
-//CORS
+//CORS We can only serve one Access control header for right now.
 server.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://www.forgetfulfairy.com");
-    res.header("Access-Control-Allow-Origin", "http:/localhost:8000");
-    res.header("Access-Control-Allow-Origin", "http:/localhost:9000");
     res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
     next();
