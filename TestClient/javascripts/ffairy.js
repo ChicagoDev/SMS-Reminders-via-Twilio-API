b49a257b345c9658ffbb35046263b3eeb52eacb4
Blake Gideon <bgideon@luc.edu>
2015-02-26 13:04:44 -0600

    Major update. Added an entire DeployClient directory to manage the difficulty between managing local testing and stable deployments. The DeployClient directory is able to be rsync'd with the server, so any changes made in the Test directory can be easily staged in Deploy client before they are pushed to the server.

---
+++ TestClient/javascripts/ffairy.js
@@ -0,0 +1,52 @@
+/**
+ * Created by bjg on 12/20/14.
+ */
+function appointmentFactory() {
+    var eventName,dateTime,phoneNumber;
+
+    eventName = $('#event').val();
+    phoneNumber = $('#phone').val();
+    dateTime = getAppointmentDate();
+
+    return {
+        eventName: eventName,
+        time: dateTime,
+        phoneNumber: phoneNumber
+    }
+}
+
+function jsonAppointment() {
+    return JSON.stringify(appointmentFactory());
+}
+
+function parseTimeToMilli(time) {
+    var timeStrings = time.split(':');
+    var milliPerHour = 3600000;
+    var milliPerMinute = 60000;
+    var hours = parseInt(timeStrings[0]);
+    var minutes = parseInt(timeStrings[1]);
+    hours *= milliPerHour;
+    minutes *= milliPerMinute;
+    //total time
+    return hours+minutes;
+
+}
+
+//YAY! Works!
+function getAppointmentDate() {
+    var apptDateAndTime = new Date($('#datepicker').datepicker('getDate').setMilliseconds(parseTimeToMilli($('#time').val())));
+    return apptDateAndTime;
+}
+
+function postAppointment() {
+    $.ajax({
+        url: 'http://localhost:3000/appointments',
+        type: 'POST',
+        data: jsonAppointment(),
+        contentType: 'application/json',
+        crossDomain: true
+
+    });
+
+    alert("Thank You! Plenty of Reminders will be sent your way.");
+}
